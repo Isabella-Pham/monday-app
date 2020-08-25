25ab1bb35333fd03311c205beea8fd5dd4b53b64
Isabella Pham <isabella.aivy.pham@gmail.com>
2020-08-24 21:15:55 -0400

    fixed some stuff

--- src/mondayClient.js
+++ src/mondayClient.js
@@ -8,8 +8,10 @@ class mondayClient {
     constructor() {
         this.monday = mondaySdk();
         this.api_key = process.env.REACT_APP_MONDAY_TOKEN;
-        this.monday.storage.instance.setItem("all_graphs", null);
+        this.setAllGraphs();
+        this.sleep(10000);
     }
+    /*
     async post(apiQuery) {
         try {
             const res = await fetch(this.endpoint, {
@@ -28,30 +30,58 @@ class mondayClient {
         } catch (error) {
             console.error(error);
         }
+    }*/
+    sleep(delay) {
+        var start = new Date().getTime();
+        while (new Date().getTime() < start + delay);
     }
+
     getTeammates() {
         const teammates = this.monday.api("{users{name,email,photo_original}}").then(res => {
             return res;
         });
         return teammates;
     }
+
     async getAllGraphs() {
         const allGraphs = this.monday.storage.instance.getItem("all_graphs").then(res => {
             return res["data"]["value"];
         });
         return allGraphs;
     }
+
+    deleteAllGraphs() {
+        const success = this.monday.storage.instance.setItem("all_graphs", "null").then(res => {
+            return res["data"]["success"];
+        });
+        return success;
+    }
+
     saveGraph(graphName, graphJSON) {
-        this.monday.storage.instance.setItem(graphName, JSON.stringify( graphJSON ));
-        this.monday.storage.instance.getItem("all_graphs").then(res => {
+        const success = this.monday.storage.instance.getItem("all_graphs").then(res => {
             const data = res["data"];
-            if (data["value"] == null) {
+            var graphList = data["value"];
+            if (graphName.includes(",")) {
+                console.log("Invalid graph name, name must not contain any commmas");
+                return false;
+            }
+            if (data["value"] == null || data["value"].toString().localeCompare("null")) {
                 this.monday.storage.instance.setItem("all_graphs", graphName);
+                return true;
             } else {
-                var graphList= data["value"] + "," + graphName;
-                this.monday.storage.instance.setItem("all_graphs", graphList);
+                if (graphList.split(",").includes(graphName)) {
+                    console.log("Graph by the name of '" + graphName + "' already exists");
+                    return false;
+                } else {
+                    this.monday.storage.instance.setItem(graphName, JSON.stringify(graphJSON));
+                    console.log("else statement graph list: " + graphList);
+                    graphList = graphList + "," + graphName;
+                    this.monday.storage.instance.setItem("all_graphs", graphList);
+                    return true;
+                }
             }
         });
+        return success;
     }
     async getGraph(graphName) {
         const graphJSON = await this.monday.storage.instance.getItem(graphName).then(res => {
@@ -62,13 +92,24 @@ class mondayClient {
     }
     async containsGraph(graphName) {
         const contains = this.monday.storage.instance.getItem(graphName).then(res => {
-            if (!res["data"]["success"]){
+            if (!res["data"]["success"] || res["data"]["value"] == null){
                 return false;
             } else {
                 return true;
             }
         });
         return contains;
+    }
+
+    setAllGraphs() {
+        const success = this.containsGraph("all_graphs").then(res => {
+            if (res == false) {
+                this.monday.storage.instance.setItem("all_graphs", "null");
+                return true;
+            } else {
+                return true;
+            }
+        });
     }
 }
 
